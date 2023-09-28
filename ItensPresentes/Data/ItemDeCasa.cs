bd399d43cf5bac4a341688b899127f8b7f322aaa
Pedro Gaudio <pedro.gaudio@inetum.com>
2023-09-28 14:27:16 -0300

    novos ajustes

--- ItensPresentes/Data/ItemDeCasa.cs
+++ ItensPresentes/Data/ItemDeCasa.cs
@@ -16,6 +16,8 @@ namespace ItensPresentes.Data
         public string NomeItem { get; set; }
         [Column("nome_pessoas")]
         public List<string> NomePessoas { get; set; }
+        [Column("ativo")]
+        public bool Ativo { get; set; }
 
     }
 }
