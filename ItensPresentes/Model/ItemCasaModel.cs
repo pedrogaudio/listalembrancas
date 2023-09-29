eb798f104d32ed646774917092478b5d79796245
Pedro Gaudio <pedro.gaudio@inetum.com>
2023-09-29 12:23:34 -0300

    ajustes

---
+++ ItensPresentes/Model/ItemCasaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ItensPresentes.Model
+{
+    public class ItemCasaModel
+    {
+        public long Id { get; set; }
+        public string NomeItem { get; set; }
+        public bool Ativo { get; set; }
+        public int Ambiente { get; set; }
+        public List<string> NomePessoas { get; set; }
+        public long Quantidade { get; set; }
+    }
+}
